77beaa0ddca8deaeb0d9bb43dc343a3d8d35fe2e
sobadon <sbdn@chasoba.net>
2021-08-19 16:32:30 +0900

    テストケースを追加

--- main_test.go
+++ main_test.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 func Test_termParser(t *testing.T) {
@@ -278,6 +281,182 @@ func Test_standardRegistrationYearParser(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("standardRegistrationYearParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_periodParser(t *testing.T) {
+	type args struct {
+		periodString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		// とてもシンプル
+		{
+			name: "1 コマ",
+			args: args{
+				periodString: "月1",
+			},
+			want:    []string{"月1"},
+			wantErr: false,
+		},
+
+		// 同じ曜日
+		{
+			name: "同じ曜日で n コマ連続",
+			args: args{
+				periodString: "月12",
+			},
+			want:    []string{"月1", "月2"},
+			wantErr: false,
+		},
+		{
+			name: "同じ曜日に x コマ飛ばして y コマ存在",
+			args: args{
+				periodString: "月1-3,5",
+			},
+			want:    []string{"月1", "月2", "月3", "月5"},
+			wantErr: false,
+		},
+
+		// 曜日が異なる
+		{
+			name: "曜日が異なり n コマ",
+			args: args{
+				periodString: "月1火1",
+			},
+			want:    []string{"月1", "火1"},
+			wantErr: false,
+		},
+		{
+			name: "曜日が異なり同じ時間帯に 1 コマ",
+			args: args{
+				periodString: "月・火1",
+			},
+			want:    []string{"月1", "火1"},
+			wantErr: false,
+		},
+		{
+			name: "曜日が異なり同じ時間帯 n コマ",
+			args: args{
+				periodString: "月・木1-3",
+			},
+			want:    []string{"月1", "木1", "月2", "木2", "月3", "木3"},
+			wantErr: false,
+		},
+		{
+			name: "曜日が異なり同じ時間帯 n コマ（1 日の中で時間が飛ぶ）",
+			args: args{
+				periodString: "月,火3,5-7",
+			},
+			want:    []string{"月3", "月5", "月6", "月7", "火3", "火5", "火6", "火7"},
+			wantErr: false,
+		},
+
+		// 応談
+		// 応談と随時、集中はすべて同じ処理をしているので↓が成功すればよい
+		{
+			name: "完全に応談のみ",
+			args: args{
+				periodString: "応談",
+			},
+			want:    []string{"応談"},
+			wantErr: false,
+		},
+		{
+			name: "完全に随時のみ",
+			args: args{
+				periodString: "随時",
+			},
+			want:    []string{"随時"},
+			wantErr: false,
+		},
+		{
+			name: "完全に集中のみ",
+			args: args{
+				periodString: "集中",
+			},
+			want:    []string{"集中"},
+			wantErr: false,
+		},
+		{
+			name: "応談と集中",
+			args: args{
+				periodString: "応談 集中",
+			},
+			want:    []string{"応談", "集中"},
+			wantErr: false,
+		},
+		{
+			name: "応談に時限がある（？）",
+			args: args{
+				periodString: "応談78",
+			},
+			want:    []string{"応談", "応談7", "応談8"},
+			wantErr: false,
+		},
+		{
+			name: "応談と通常コマ",
+			args: args{
+				periodString: "応談 木1",
+			},
+			want:    []string{"応談", "木1"},
+			wantErr: false,
+		},
+		{
+			name: "応談に時限があるものと通常コマ",
+			args: args{
+				periodString: "応談78 木1",
+			},
+			want:    []string{"応談", "応談7", "応談8", "木1"},
+			wantErr: false,
+		},
+
+		// 区切り文字
+		{
+			name: "曜日またぎの区切り文字がない",
+			args: args{
+				periodString: "月1火1",
+			},
+			want:    []string{"月1", "火1"},
+			wantErr: false,
+		},
+		{
+			name: "曜日またぎの区切り文字がスペース",
+			args: args{
+				periodString: "月1 火1",
+			},
+			want:    []string{"月1", "火1"},
+			wantErr: false,
+		},
+		{
+			name: "曜日またぎの区切り文字が中黒",
+			args: args{
+				periodString: "月1・火1",
+			},
+			want:    []string{"月1", "火1"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := periodParser(tt.args.periodString)
+
+			// slice の順序は重視していないためソートして両方あわせる
+			sort.Strings(got)
+			sort.Strings(tt.want)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("periodParser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("periodParser() = %v, want %v", got, tt.want)
 			}
 		})
 	}
